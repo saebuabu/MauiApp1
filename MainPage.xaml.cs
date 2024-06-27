50b5d8e934810e03ec865b1989370d9239d485da
saebuabu <a.saebu@kw1c.nl>
2024-06-27 22:50:56 +0200

    Tweede pagina en tweede knop toegevoegd aanb de homnepage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,6 +20,11 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+        // Method that implents a button cluck to GotoPage1Button
+        private async void OnGotoPage1Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageStacklayout());
+        }
     }
 
 }
---
+++ PageStacklayout.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class PageStacklayout : ContentPage
+{
+	public PageStacklayout()
+	{
+		InitializeComponent();
+	}
+}
