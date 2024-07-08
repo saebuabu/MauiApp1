5ae09b10ae642d2dd154623d302121b20461648f
saebuabu <a.saebu@kw1c.nl>
2024-07-08 15:54:59 +0200

    Eerste XAML pagina met een verticale stacklayout en verschillenmde controls en een grid

--- PageStacklayout.xaml.cs
+++ PageStacklayout.xaml.cs
@@ -2,8 +2,30 @@ namespace MauiApp1;
 
 public partial class PageStacklayout : ContentPage
 {
-	public PageStacklayout()
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    public PageStacklayout()
 	{
 		InitializeComponent();
-	}
+        if (File.Exists(_fileName))
+        {
+            editor.Text = File.ReadAllText(_fileName);
+        }
+    }
+
+    private void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, editor.Text);
+        message.Text = "Aantekening is opgeslagen";
+    }
+
+    private void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+            editor.Text = string.Empty;
+            message.Text = "Aantekening verwijderd";
+        }
+    }
+
 }
